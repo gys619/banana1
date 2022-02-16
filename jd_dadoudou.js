f7b36994724ed9d47912ae74e6516dd367d5ef16
gys619 <1554537386@qq.com>
2022-02-17 02:12:18 +0800

    删除：[ backup/.DS_Store, backup/gua_wealth_island_help.js, backup/jd_car.js, backup/jd_car_exchange.js, backup/jd_mpdzcar.js, backup/jd_mpdzcar_game.js, backup/jd_mpdzcar_help.js, jd_zsign.js] 修改内容：[ jd_dadoudou.js]

--- jd_dadoudou.js
+++ jd_dadoudou.js
@@ -2,6 +2,7 @@
 https://lzkj-isv.isvjcloud.com/wxgame/activity/8530275?activityId=e5cff304b4b545a98ba6130ceb4027d2
 爆裂豆豆游戏
 活动ID环境变量 WXGAME_ACT_ID
+7 7 7 7 7 jd_dadoudou.js
 */
 const $ = new Env('打豆豆');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
