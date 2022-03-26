4e14996bb32e83eeae1fd91a7ffd5d941dbbcf4b
gys619 <1554537386@qq.com>
2022-03-26 16:32:12 +0800

    新增：[ jd_opencardL104.js] 修改内容：[ jd_opencardL100.js, jd_opencardL101.js, jd_opencardL102.js, jd_opencardL98.js] 修改文件名：[ jd_opencardL99.js -> activity/jd_opencardL99.js]

--- activity/jd_opencardL99.js
+++ activity/jd_opencardL99.js
@@ -47,7 +47,7 @@ if ($.isNode()) {
         $.msg($.name, "【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取", "https://bean.m.jd.com/bean/signIndex.action", { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
         return;
     }
-  authorCodeList = await getAuthorCodeList('https://gitee.com/444444521/JD-Scripts/raw/master/shareCodes/opencard99.json')
+  authorCodeList = await getAuthorCodeList('https://gitee.com/KingRan521/JD-Scripts/raw/master/shareCodes/opencard99.json')
   if ($.getAuthorCodeListerr === false) {
       authorCodeList = [
           'ee9c70d1abca4b1ab21ec1ed0a51dcb9',
